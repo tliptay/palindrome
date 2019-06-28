fb478b0f4334a3f0a57d814020106ff059b8b39d
Tom L <tliptay@gmail.com>
2019-06-28 17:12:45 +0000

    Add readme

--- index.js
+++ index.js
@@ -1,49 +1,21 @@
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
-}
-
-String.prototype.blank = function() {
-  return !!this.match(/^\s*$/);
-}
-
-Array.prototype.last = function() {
-  return this[this.length - 1];
-}
-
-
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
 }
 
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function (string) {
-    return string.toLowerCase();
-  }
-
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.content.toLowerCase();
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // Returns translation processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.processor(this.translation);
-  }
-}
-
-TranslatedPhrase.prototype = new Phrase();
